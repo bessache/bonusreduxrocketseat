31337ff6844e9adda653fa7680079cec20d518fa
Renato Bessa <bessache@outlook.com>
2020-09-06 12:13:17 -0300

    commita aula tipagem no redux

--- src/store/modules/cart/reducer.ts
+++ src/store/modules/cart/reducer.ts
@@ -5,7 +5,6 @@ const INITIAL_STATE: ICartState = {
     items: []
 };
 
-
 const cart: Reducer<ICartState> = (state=INITIAL_STATE, action) => {
     switch(action.type) {
         case 'ADD_PRODUCT_TO_CART':{
