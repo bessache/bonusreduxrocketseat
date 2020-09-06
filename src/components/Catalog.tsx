190fe4c40115fce1298170f2774d71050afacfe6
Renato Bessa <bessache@outlook.com>
2020-09-06 10:54:28 -0300

    comitt inicial ja na parte criando o servidor json

---
+++ src/components/Catalog.tsx
@@ -0,0 +1,30 @@
+import React, { useState, useEffect } from 'react';
+
+import { IProduct } from '../store/modules/cart/types';
+import api from '../services/api';
+
+const Catalog: React.FC = () => {
+    const [catalog, setCatalog]=useState<IProduct[]>([]);
+
+    useEffect(()=>{
+        api.get('products').then(response => {
+            setCatalog(response.data)
+        })
+    },[]);
+
+    return (
+        <main>
+            <h1>Catalog</h1>
+
+            {catalog.map(product=>(
+                <article key={product.id}>
+                    <strong>{product.title}</strong> {" - "}
+                    <span>{product.price}</span> {"  "}
+                    <button type="button">Comprar</button>
+                </article>
+            ))}
+        </main>
+        
+    );
+}
+export default Catalog;
